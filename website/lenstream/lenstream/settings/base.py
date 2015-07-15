d77d5b53d5911b1c9320f498db53dd3ee88781b6
Aaron Yong <aaron@liftinteractive.com>
2015-07-15 00:38:25 -0600

    Add pagination and serve media in DEBUG

--- website/lenstream/lenstream/settings/base.py
+++ website/lenstream/lenstream/settings/base.py
@@ -134,6 +134,8 @@ REST_FRAMEWORK = {
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 # Django OAuth Toolkit
